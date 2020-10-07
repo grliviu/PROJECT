bf65ed4f216d9dcdb5f872baaab784195946a8ee
GRAUR_NAME LIVIU_NAME <oliviu66@yahoo.ro>
2020-10-07 22:17:57 +0300

    commit

---
+++ Selenium/src/test/java/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+       //WishList wishlist = new WishList();
+       // wishlist.addToWishlist();
+        //RegisterTest registration = new RegisterTest();
+        //registration.register();
+        LoginTest login = new LoginTest();
+        login.loginTestMethod1();
+
+
+
+
+    }
+}
